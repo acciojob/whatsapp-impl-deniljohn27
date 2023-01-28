2c3ccd727f05a1f96e839ecad99c51535471e106
denil <deniljohnms27@gmail.com>
2023-01-28 17:26:58 +0530

    second5Try

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -5,9 +5,16 @@ import java.util.Date;
 public class Message {
     private int id;
     private String content;
-    private Date timestamp =new Date();;
+    private Date timestamp;
 
+    public Message(){
+        this.timestamp = new Date();
+    }
 
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
 
     public int getId() {
         return id;
